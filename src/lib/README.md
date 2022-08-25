eb306ec62e180fe59cefb2ff6bd005367112169a
Emmanuel Sogelola <esogelola@gmail.com>
2022-08-24 20:55:07 -0400

    Initial Firebase Auth Integration
    Further changes will be made

---
+++ src/lib/README.md
@@ -0,0 +1,13 @@
+# Libraries
+
+Contains hooks, initialization and any utility classes
+
+
+
+References:
+https://blog.logrocket.com/implementing-authentication-in-next-js-with-firebase/
+https://github.com/deltahacks/judge/blob/master/src/firebaseinit.ts
+https://firebase.google.com/docs/auth/web/start#web-version-9
+https://blog.jarrodwatts.com/the-ultimate-guide-to-firebase-with-nextjs
+https://blog.logrocket.com/nextjs-cloud-firestore-full-stack-app-tutorial/
+https://github.com/fireship-io/next-firebase-course/blob/main/lib/firebase.js
---
+++ src/lib/hooks/useFirebaseAuth.ts
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+import Firebase from "../firebase";
+import { User } from "@firebase/auth-types";
+
+type AuthUser = {
+  uid: string | null;
+  email: string | null;
+};
+
+const formatAuthUser = (user: User) => ({
+  uid: user.uid,
+  email: user.email,
+});
+
+export default function useFirebaseAuth() {
+  const [authUser, setAuthUser] = useState<AuthUser | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const authStateChanged = async (authState: User | null) => {
+    if (!authState) {
+      setAuthUser(null);
+      setLoading(false);
+      return;
+    }
+
+    setLoading(true);
+    const formattedUser = formatAuthUser(authState);
+    setAuthUser(formattedUser);
+    setLoading(false);
+  };
+
+  const clear = () => {
+    setAuthUser(null);
+    setLoading(true);
+  };
+
+  const signInWithEmailAndPassword = (email: string, password: string) =>
+    Firebase.auth().signInWithEmailAndPassword(email, password);
+
+  const createUserWithEmailAndPassword = (email: string, password: string) =>
+    Firebase.auth().createUserWithEmailAndPassword(email, password);
+
+  const signOut = () => Firebase.auth().signOut().then(clear);
+
+  useEffect(() => {
+    const unsubscribe = Firebase.auth().onAuthStateChanged(authStateChanged);
+    return () => unsubscribe();
+  }, []);
+
+  return {
+    authUser,
+    loading,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    signOut,
+  };
+}
