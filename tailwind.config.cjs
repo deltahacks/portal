0e6a64e75708d8a2e9707fd3d900b909bb20193e
Nivetha Kuruparan <nivetha.kuruparan@gmail.com>
2022-09-03 12:09:56 -0400

    feat: created pre-registration page and social buttons component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
