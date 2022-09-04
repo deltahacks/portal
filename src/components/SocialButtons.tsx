dea9959e06bc59fbf301cc75cd9f553c7cd3ba0c
Krish <52761222+Krish120003@users.noreply.github.com>
2022-09-03 22:06:15 -0400

    Use SVGs over PNGs for social icons

--- src/components/SocialButtons.tsx
+++ src/components/SocialButtons.tsx
@@ -1,38 +1,41 @@
-import Image from 'next/image'
-
+import Image from "next/image";
 
 export default function SocialButtons() {
-    return (
-        <div className="flex gap-x-2.5 md:grid md:auto-cols-max md:gap-y-2.5">
-            <a href="https://www.instagram.com/deltahacks/" className="filter hover:brightness-150">
-                <SocialIcon src="/Instagram.png" alt="Instgram.png"/>
-            </a>            
-            <a href="https://www.facebook.com/thedeltahacks/" className="filter hover:brightness-150">
-                <SocialIcon src="/Facebook.png" alt="Facebook.png"/>
-            </a>
-            <a href="https://twitter.com/deltahacks" className="filter hover:brightness-150">
-                <SocialIcon src="/Twitter.png" alt="Twitter.png"/>
-            </a>
-            <a href="https://www.linkedin.com/company/deltahacks/" className="filter hover:brightness-150">
-                <SocialIcon src="/LinkedIn.png" alt="LinkedIn.png"/>
-            </a>
-        </div>
-    )
+  return (
+    <div className="flex gap-x-2.5 md:grid md:auto-cols-max md:gap-y-2.5">
+      <a
+        href="https://www.instagram.com/deltahacks/"
+        className="filter hover:brightness-150"
+      >
+        <SocialIcon src="/Instagram.svg" alt="Instgram.png" />
+      </a>
+      <a
+        href="https://www.facebook.com/thedeltahacks/"
+        className="filter hover:brightness-150"
+      >
+        <SocialIcon src="/Facebook.svg" alt="Facebook.png" />
+      </a>
+      <a
+        href="https://twitter.com/deltahacks"
+        className="filter hover:brightness-150"
+      >
+        <SocialIcon src="/Twitter.svg" alt="Twitter.png" />
+      </a>
+      <a
+        href="https://www.linkedin.com/company/deltahacks/"
+        className="filter hover:brightness-150"
+      >
+        <SocialIcon src="/LinkedIn.svg" alt="LinkedIn.png" />
+      </a>
+    </div>
+  );
 }
 
 type SocialIconProps = {
-    src: string
-    alt: string
-}
+  src: string;
+  alt: string;
+};
 
 function SocialIcon({ src, alt }: SocialIconProps) {
-    return (
-        <Image
-            src={src}
-            alt={alt}
-            width="41px"
-            height="41px"
-        />
-    )
+  return <Image src={src} alt={alt} width="41px" height="41px" />;
 }
-
