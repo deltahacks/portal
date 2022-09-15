e7c07db6fbe2560a40114ba27c51c154fec82194
ronald-luo <r.luo.180@gmail.com>
2022-09-15 18:50:35 -0400

    add mobile view changes

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Background = () => {
     return (
-        <div className="animate-pulse absolute top-0 left-0 h-full w-full bg-cover -z-10" style={{backgroundImage: `url(./background.svg)`}}></div>
+        <div className="bg-[#1F1F1F] absolute top-0 h-screen w-screen -z-10">
+            <div className="animate-pulse absolute top-0 right-0 h-full w-full bg-cover -z-10" style={{backgroundImage: `url(./background.svg)`}}></div>
+        </div>
     );
 };
 
--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const NavBar = () => {
     return (
-        <div className="flex flex-row justify-between mt-8 mx-10 items-center">
+        <div className="flex flex-row justify-between mt-5 mx-9 md:mt-8 md:mx-10 items-center">
 
             <div className="flex flex-row justify-between items-center">
                 <a className="mr-5" href="#">
-                    <img className="inline-block w-20 h-16" src="logo.svg"/>
+                    <img className="inline-block w-12 h-10 md:w-20 md:h-16" src="logo.svg"/>
                 </a>
 
-                <div className="inline-block font-main">
+                <div className="hidden md:inline-block font-main">
                     <h1 className="text-2xl">
                         <strong>Delta</strong>Hacks <strong>IX</strong>
                     </h1>
@@ -18,23 +18,27 @@ const NavBar = () => {
                     </p>
                 </div>
 
-                <div className="font-sub ml-5">
+                <div className="hidden md:flex font-sub ml-5">
                     <a className="text-base font-bold mx-2" href="#">Dashboard</a>
 
                     <a className="text-base font-bold mx-2" href="#">Calendar</a>
                 </div>
             </div>
             
-            <div>
+            <div className="flex items-center">
                 <button className="mx-2">
-                    <img className="inline-block w-5 h-5" src="dark-toggle.svg"/>
+                    <img className="hidden md:inline-block w-5 h-5" src="dark-toggle.svg"/>
                 </button>
 
-                <a className="font-sub text-sm mx-2">
-                    Logged in as <strong className=" font-bold"> Username</strong>
+                <a className="font-sub text-sm mr-8 mx-2 md:hidden" href="#">
+                    <strong className="font-bold"> Home</strong>
+                </a>
+                
+                <a className="hidden lg:inline-block font-sub text-sm mx-2">
+                    Logged in as <strong className="font-bold"> Username</strong>
                 </a>
 
-                <button className="bg-[#4F14EE] py-2.5 px-7 rounded font-sub text-sm font-bold mx-2">
+                <button className="bg-[#4F14EE] py-2.5 px-5 md:px-7 rounded font-sub text-sm font-bold mx-2">
                     Sign out
                 </button>
             </div>
