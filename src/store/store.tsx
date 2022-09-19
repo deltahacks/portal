e6bfe46849d7bb5ae25b7ce1bac10dc5e537d5a2
jeffreychan <kkjeffreychan@gmail.com>
2022-09-19 12:39:56 -0400

    implementing zustand

---
+++ src/store/store.tsx
@@ -0,0 +1,18 @@
+import create from "zustand"
+import { devtools } from "zustand/middleware"
+
+interface TestingStore {
+  words: string[]
+  addWord: (word: string) => void
+}
+
+const useStore = create<TestingStore>()(
+  devtools(
+    (set) => ({
+      words: ["hi", "hello"],
+      addWord: (word: string) => set((state) => ({ words: [...state.words, word] }))
+    })
+    , { serialize: true })
+)
+
+export default useStore
