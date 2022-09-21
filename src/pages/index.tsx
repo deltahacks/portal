63e6c305fcb5ebb72bec2273cf0b3a1be37cfed7
Krish <52761222+Krish120003@users.noreply.github.com>
2022-09-21 11:02:01 -0400

    Run linter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,9 @@ import { signIn } from "next-auth/react";
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen space-y-6">
-      <div className="btn btn-primary" onClick={() => signIn()}>Sign In</div>
+      <div className="btn btn-primary" onClick={() => signIn()}>
+        Sign In
+      </div>
       {/* <Link href={"/Login"}>Login</Link>
       <Link href={"/SignUp"}>Sign Up</Link>
       <Link href={"/ForgotPassword"}>Forgot Password</Link> */}
