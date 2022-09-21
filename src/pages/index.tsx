e14a68a867685e816cb60de2619f12a1d124fe0b
Krish <52761222+Krish120003@users.noreply.github.com>
2022-09-21 11:18:40 -0400

    show username

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,17 @@
 import type { NextPage } from "next";
 import React from "react";
 import Link from "next/link";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
+  const session = useSession();
+
   return (
     <div className="flex flex-col items-center justify-center h-screen space-y-6">
       <div className="btn btn-primary" onClick={() => signIn()}>
         Sign In
       </div>
+      <div className="p-4 text-4xl">Hello {session?.data?.user?.name}</div>
       {/* <Link href={"/Login"}>Login</Link>
       <Link href={"/SignUp"}>Sign Up</Link>
       <Link href={"/ForgotPassword"}>Forgot Password</Link> */}
