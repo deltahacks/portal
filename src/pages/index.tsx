9d10ddd16c758f49823e5e5a0d76c1c1f2e37646
Krish <52761222+Krish120003@users.noreply.github.com>
2022-09-21 12:53:40 -0400

    force redirect unauthenticated users

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,32 @@
-import type { NextPage } from "next";
+import type { GetServerSidePropsContext, NextPage } from "next";
 import React from "react";
 import Link from "next/link";
-import { signIn, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { getServerAuthSession } from "../server/common/get-server-auth-session";
 
 const Home: NextPage = () => {
   const session = useSession();
 
   return (
     <div className="flex flex-col items-center justify-center h-screen space-y-6">
-      <div className="btn btn-primary" onClick={() => signIn()}>
-        Sign In
-      </div>
-      <div className="p-4 text-4xl">Hello {session?.data?.user?.name}</div>
+      Hello to dashboard?
+      {session.data?.user?.email}
+      <button className="btn btn-error" onClick={() => signOut()}>
+        Sign Out
+      </button>
       {/* <Link href={"/Login"}>Login</Link>
       <Link href={"/SignUp"}>Sign Up</Link>
       <Link href={"/ForgotPassword"}>Forgot Password</Link> */}
     </div>
   );
+};
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  const session = await getServerAuthSession(ctx);
+  if (!session) {
+    return { redirect: { destination: "/login", permanent: false } };
+  }
+  return { props: {} };
 };
 
 export default Home;
