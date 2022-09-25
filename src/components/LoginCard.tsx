b7a5c560796d665515bb3d4c69db8e21e11699b4
jeffreychan <kkjeffreychan@gmail.com>
2022-09-25 19:09:10 -0400

    updated button component to support main login button

--- src/components/LoginCard.tsx
+++ src/components/LoginCard.tsx
@@ -1,4 +1,4 @@
-import google_icon from "../../public/images/google_icon.svg";
+import google_logo from "../../public/images/google_logo.svg";
 import github_logo from "../../public/images/github_logo.svg";
 import discord_logo from "../../public/images/discord_logo.svg";
 import { signIn } from "next-auth/react";
@@ -7,26 +7,33 @@ import Image from "next/image";
 interface LoginProps {
   image: any;
   title: string;
+  main: boolean;
 }
 
 const buttons = {
   GitHub: {
     image: github_logo,
+    main: false
   },
   Discord: {
     image: discord_logo,
+    main: false
   },
 };
 
 const LoginButton = (prop: LoginProps) => {
-  const { title, image } = prop;
+  const { title, image, main } = prop;
   const normalizedTitle = title.toLowerCase();
+  const buttonStyling = main ?
+    "align-items-center flex h-32 items-center justify-center rounded-lg bg-[#4F14EE] py-6 text-center text-xl font-medium" :
+    "text-l flex items-center justify-center rounded-md border border-zinc-700 py-2 text-center font-medium hover:bg-zinc-700"
+  const iconStyling = main ? "mr-4 aspect-square w-8" : "mr-2 aspect-square  w-4"
   return (
     <button
-      className="text-l flex items-center justify-center rounded-md border border-zinc-700 py-2 text-center font-medium hover:bg-zinc-700"
+      className={buttonStyling}
       onClick={() => signIn(normalizedTitle)}
     >
-      <div className="mr-2 aspect-square  w-4">
+      <div className={iconStyling}>
         <Image src={image} alt={`${title} Logo`} layout="responsive"></Image>
       </div>
       {title}
@@ -41,28 +48,14 @@ const LoginCard = () => {
         Log In
       </h2>
       <div className="flex flex-col gap-1 rounded-xl border border-zinc-600 bg-zinc-800 p-4 text-white">
-        <button
-          className="align-items-center flex h-32 items-center justify-center rounded-lg bg-[#4F14EE] py-6 text-center text-xl font-medium"
-          onClick={() => signIn("google")}
-        >
-          <div className="mr-4 aspect-square w-8">
-            <Image
-              src={google_icon}
-              alt="Google Login Icon"
-              layout="responsive"
-            ></Image>
-          </div>
-          <div className="whitespace-nowrap md:text-lg">
-            Sign in with Google
-          </div>
-        </button>
+        <LoginButton key={"google"} title={"Sign in with Google"} image={google_logo} main={true} />
         <div className="my-2 flex items-center justify-center text-center text-sm text-zinc-600">
           <span className="h-0.5 w-full bg-zinc-600" />
           <div className="mx-2 whitespace-nowrap">Or Continue With</div>
           <span className="h-0.5 w-full bg-zinc-600" />
         </div>
         {Object.entries(buttons).map(([key, value]) => (
-          <LoginButton key={key} title={key} image={value.image} />
+          value.main ? null : <LoginButton key={key} title={key} image={value.image} main={value.main} />
         ))}
       </div>
     </div>
