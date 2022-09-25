287ff66fd614789ed68f795e3f310a6be4a0be24
jeffreychan <kkjeffreychan@gmail.com>
2022-09-25 19:13:00 -0400

    ran formatter

--- src/components/LoginCard.tsx
+++ src/components/LoginCard.tsx
@@ -13,26 +13,25 @@ interface LoginProps {
 const buttons = {
   GitHub: {
     image: github_logo,
-    main: false
+    main: false,
   },
   Discord: {
     image: discord_logo,
-    main: false
+    main: false,
   },
 };
 
 const LoginButton = (prop: LoginProps) => {
   const { title, image, main } = prop;
   const normalizedTitle = title.toLowerCase();
-  const buttonStyling = main ?
-    "align-items-center flex h-32 items-center justify-center rounded-lg bg-[#4F14EE] py-6 text-center text-xl font-medium" :
-    "text-l flex items-center justify-center rounded-md border border-zinc-700 py-2 text-center font-medium hover:bg-zinc-700"
-  const iconStyling = main ? "mr-4 aspect-square w-8" : "mr-2 aspect-square  w-4"
+  const buttonStyling = main
+    ? "align-items-center flex h-32 items-center justify-center rounded-lg bg-[#4F14EE] py-6 text-center text-xl font-medium"
+    : "text-l flex items-center justify-center rounded-md border border-zinc-700 py-2 text-center font-medium hover:bg-zinc-700";
+  const iconStyling = main
+    ? "mr-4 aspect-square w-8"
+    : "mr-2 aspect-square  w-4";
   return (
-    <button
-      className={buttonStyling}
-      onClick={() => signIn(normalizedTitle)}
-    >
+    <button className={buttonStyling} onClick={() => signIn(normalizedTitle)}>
       <div className={iconStyling}>
         <Image src={image} alt={`${title} Logo`} layout="responsive"></Image>
       </div>
@@ -48,15 +47,27 @@ const LoginCard = () => {
         Log In
       </h2>
       <div className="flex flex-col gap-1 rounded-xl border border-zinc-600 bg-zinc-800 p-4 text-white">
-        <LoginButton key={"google"} title={"Sign in with Google"} image={google_logo} main={true} />
+        <LoginButton
+          key={"google"}
+          title={"Sign in with Google"}
+          image={google_logo}
+          main={true}
+        />
         <div className="my-2 flex items-center justify-center text-center text-sm text-zinc-600">
           <span className="h-0.5 w-full bg-zinc-600" />
           <div className="mx-2 whitespace-nowrap">Or Continue With</div>
           <span className="h-0.5 w-full bg-zinc-600" />
         </div>
-        {Object.entries(buttons).map(([key, value]) => (
-          value.main ? null : <LoginButton key={key} title={key} image={value.image} main={value.main} />
-        ))}
+        {Object.entries(buttons).map(([key, value]) =>
+          value.main ? null : (
+            <LoginButton
+              key={key}
+              title={key}
+              image={value.image}
+              main={value.main}
+            />
+          )
+        )}
       </div>
     </div>
   );
