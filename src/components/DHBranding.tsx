c7c9b5ee551da6afac660ca90aef9c32ceba642c
jeffreychan <kkjeffreychan@gmail.com>
2022-09-25 21:31:01 -0400

    ran formatter

--- src/components/DHBranding.tsx
+++ src/components/DHBranding.tsx
@@ -6,11 +6,7 @@ const DHBranding = () => {
   return (
     <div className="flex items-center gap-4">
       <div className="md:w-30 hidden aspect-square w-20 md:block">
-        <Image
-          src={logo}
-          alt="DeltaHacks logo"
-          layout="responsive"
-        ></Image>
+        <Image src={logo} alt="DeltaHacks logo" layout="responsive"></Image>
       </div>
       <div className="text-white">
         <h1 className="whitespace-nowrap font-montserrat text-4xl font-bold md:text-4xl lg:text-5xl">
