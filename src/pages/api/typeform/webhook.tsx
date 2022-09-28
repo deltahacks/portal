0acaac0433663b15028c99a9a7dcca2073772e56
jeffreychan <kkjeffreychan@gmail.com>
2022-09-28 12:48:03 -0400

    test

--- src/pages/api/typeform/webhook.tsx
+++ src/pages/api/typeform/webhook.tsx
@@ -2,7 +2,10 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { env } from "../../../env/server.mjs";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const body = JSON.parse(req.body)
-  console.log(body);
-  return body
+  if (req.method === 'POST') {
+    console.log(JSON.parse(req.body))
+    return JSON.parse(req.body)
+  } else {
+    return { error: "You can only make post" }
+  }
 }
