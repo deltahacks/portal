dbd110974133d349ae35ca579b0f39812b1a3371
jeffreychan <kkjeffreychan@gmail.com>
2022-09-28 12:29:37 -0400

    test

--- src/pages/api/typeform/webhook.tsx
+++ src/pages/api/typeform/webhook.tsx
@@ -1,12 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { env } from "../../../env/server.mjs";
 
+let stuff: any[] = []
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
-    res.status(405).send({ message: 'Only POST requests allowed' })
-    return
-  }
   const body = JSON.parse(req.body)
+  console.log(body);
+  stuff.push(body);
   return body
 }
