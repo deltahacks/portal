a0c88a8df93448d2142a89e10cb2778deff2d622
jeffreychan <kkjeffreychan@gmail.com>
2022-09-28 12:34:05 -0400

    test

--- src/pages/api/typeform/webhook.tsx
+++ src/pages/api/typeform/webhook.tsx
@@ -1,11 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { env } from "../../../env/server.mjs";
 
-let stuff: any[] = []
-
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const body = JSON.parse(req.body)
   console.log(body);
-  stuff.push(body);
   return body
 }
