058d46b0e873ad1ae018a043cbb572f932eb11af
arian81 <ahmadinejadarian@gmail.com>
2022-09-29 18:26:20 -0400

    test netlify

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,13 @@ import React from "react";
 import Link from "next/link";
 
 const Home: NextPage = () => {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen space-y-6">
-      <Link href={"/Login"}>Login</Link>
-      <Link href={"/SignUp"}>Sign Up</Link>
-      <Link href={"/ForgotPassword"}>Forgot Password</Link>
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-center justify-center h-screen space-y-6">
+            <Link href={"/Login"}>Login</Link>
+            <Link href={"/SignUp"}>Sign Up</Link>
+            <Link href={"/ForgotPassword"}>Forgot Password like a dumbass</Link>
+        </div>
+    );
 };
 
 export default Home;
