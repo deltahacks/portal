f25a98133decff2ae80da16e6023d06931831c59
adampodolak <podola2@mcmaster.ca>
2022-10-03 13:25:52 -0400

    Merge branch 'ron-6' of https://github.com/deltahacks/portal into hamburger-menu

--- src/pages/pre_registration.tsx
+++ src/pages/pre_registration.tsx
@@ -132,9 +132,9 @@ const PreRegistration: NextPage = () => {
             </main>
           </div>
         </div>
-        <div className="drawer-side">
-          <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
-          <div className="menu h-full w-80 flex-row content-between overflow-y-auto bg-[#1F1F1F] p-4">
+        <div className="drawer-side md:hidden">
+          <label htmlFor="my-drawer-3" className="drawer-overlay md:hidden"></label>
+          <div className="menu h-full w-80 flex-row content-between overflow-y-auto bg-[#1F1F1F] p-4 md:hidden">
             <ul className="w-full">
               {/* <!-- Sidebar content here --> */}
               <li>
