d770c5f49fe3002281f1c182abead0c8f7c6c717
jeffreychan <kkjeffreychan@gmail.com>
2022-10-13 01:31:08 -0400

    embeded typeform

---
+++ src/pages/apply.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+import { Widget } from '@typeform/embed-react'
+
+const Apply: NextPage = () => {
+  return (
+    <Widget id="vBfgzBit" style={{ borderRadius: 'none', width: '100%', height: '100%' }} className="rounded-none" />
+  );
+};
+
+export default Apply
