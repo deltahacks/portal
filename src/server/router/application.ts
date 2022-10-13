1efce8be41b2216f254a7b67a18f153bc59b7810
Krish <52761222+Krish120003@users.noreply.github.com>
2022-10-13 02:20:15 -0400

    add TRPC route to check if user has application

---
+++ src/server/router/application.ts
@@ -0,0 +1,21 @@
+import { createProtectedRouter } from "./context";
+
+// Example router with queries that can only be hit if the user requesting is signed in
+export const applicationRouter = createProtectedRouter().query("received", {
+  async resolve({ ctx }) {
+    const user = await prisma?.user.findFirst({
+      where: { id: ctx.session.user.id },
+    });
+
+    if (!user) {
+      return false;
+    }
+    if (
+      user.typeform_response_id === undefined ||
+      user.typeform_response_id === null
+    ) {
+      return false;
+    }
+    return true;
+  },
+});
--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,13 +2,13 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 
-import { exampleRouter } from "./example";
+import { exampleRouter } from "./exmaple";
 import { protectedExampleRouter } from "./protected-example-router";
+import { applicationRouter } from "./application";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("example.", exampleRouter)
-  .merge("auth.", protectedExampleRouter);
+  .merge("application.", applicationRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
