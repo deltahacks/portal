3b38700b6df03be5a6b8fcb4d328bbcbd8580e8c
Krish <52761222+Krish120003@users.noreply.github.com>
2022-10-13 02:49:41 -0400

    Delete background.tsx

--- src/components/background.tsx
+++
@@ -1,9 +0,0 @@
-const Background = () => {
-  return (
-    <div className="absolute inset-0 -z-10 h-full w-full overflow-hidden bg-[#eeeeee] dark:bg-[#171717]">
-      <div className="light-gradient dark:dark-gradient absolute inset-0 -top-[50%] -left-[50%] h-[200%] w-[200%] -rotate-12 animate-slow-bg"></div>
-    </div>
-  );
-};
-
-export default Background;
