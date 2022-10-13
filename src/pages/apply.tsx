2c6a1b211254c4fab1e7d4b9a6d08908ab576d9d
jeffreychan <kkjeffreychan@gmail.com>
2022-10-13 02:46:42 -0400

    updated form

--- src/pages/apply.tsx
+++ src/pages/apply.tsx
@@ -3,7 +3,7 @@ import { Widget } from '@typeform/embed-react'
 
 const Apply: NextPage = () => {
   return (
-    <Widget id="vBfgzBit" style={{ borderRadius: 'none', width: '100%', height: '100%' }} className="rounded-none" />
+    <Widget id="MVo09hRB" style={{ borderRadius: 'none', width: '100%', height: '100%' }} className="rounded-none" />
   );
 };
 
