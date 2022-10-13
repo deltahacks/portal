1956c414156dc89c87ab7ff5500def83d067051f
Jeffrey Chan <77026758+KaiKitJeffreyChan@users.noreply.github.com>
2022-10-13 02:51:45 -0400

    feat: Embeded Typeform (#26)
    * embeded typeform
    
    * make widget import dynamic
    
    * fixed import
    
    * dynamic widget
    
    * testing
    
    * updated form
    
    * format
    
    Co-authored-by: Krish <krish120003@gmail.com>

---
+++ src/pages/apply.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import { Widget } from "@typeform/embed-react";
+
+const Apply: NextPage = () => {
+  return (
+    <Widget
+      id="MVo09hRB"
+      style={{ borderRadius: "none", width: "100%", height: "100%" }}
+      className="rounded-none"
+    />
+  );
+};
+
+export default Apply;
