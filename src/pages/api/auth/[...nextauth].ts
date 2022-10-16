d0b22b8cb8b066a139398380878cf37d723c5d75
arian81 <ahmadinejadarian@gmail.com>
2022-10-15 23:19:01 -0400

    linting

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -37,7 +37,7 @@ export const authOptions: NextAuthOptions = {
     LinkedInProvider({
       clientId: env.LINKEDIN_CLIENT_ID,
       clientSecret: env.LINKEDIN_CLIENT_SECRET,
-    })
+    }),
     // ...add more providers here
   ],
 };
