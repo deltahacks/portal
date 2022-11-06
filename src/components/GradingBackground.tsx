0d2b7f2315b327904ff3c15689a01817bf198629
adampodolak <podola2@mcmaster.ca>
2022-11-05 22:41:56 -0400

    feat: newpage template for grading portal

---
+++ src/components/GradingBackground.tsx
@@ -0,0 +1,8 @@
+const GradingBackground = () => {
+    return (
+        <div className="absolute inset-0 -z-10 h-full w-full overflow-hidden bg-[#eeeeee] dark:bg-[#171717]">
+        </div>
+    );
+};
+
+export default GradingBackground;
