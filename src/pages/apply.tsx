537bd7016d7c4a7c4231464dcb6d75f60a351007
Jeffrey Chan <77026758+KaiKitJeffreyChan@users.noreply.github.com>
2022-11-11 01:45:20 -0500

    fix: router & trpc race condition (#53)
    * fix: router & trpc race condition
    
    * fix: removed error page (#54)
    
    * feat : no more crisis
    
    * linting
    
    Co-authored-by: Krish <krish120003@gmail.com>
    Co-authored-by: Arian Ahmadinejad <35879206+arian81@users.noreply.github.com>

--- src/pages/apply.tsx
+++ src/pages/apply.tsx
@@ -5,8 +5,10 @@ import { prisma } from "../server/db/client";
 import { trpc } from "../utils/trpc";
 import { appRouter } from "../server/router";
 import { createContext } from "../server/router/context";
+import { useRouter } from "next/router";
 
 const Apply: NextPage = () => {
+  const router = useRouter();
   const submitResponseId = trpc.useMutation("application.submit");
 
   return (
@@ -14,9 +16,9 @@ const Apply: NextPage = () => {
       id="MVo09hRB"
       style={{ borderRadius: "none", width: "100%", height: "100%" }}
       className="rounded-none"
-      onSubmit={(event) => {
-        submitResponseId.mutate({ id: event.responseId });
-        window.location.href = "/dashboard";
+      onSubmit={async (event) => {
+        await submitResponseId.mutateAsync({ id: event.responseId });
+        await router.push("/dashboard");
       }}
     />
   );
@@ -26,27 +28,25 @@ export const getServerSideProps = async (
   context: any,
   ctx: GetServerSidePropsContext
 ) => {
+  const session = await getServerAuthSession(context);
+
+  if (!session || !session.user) {
+    return { redirect: { destination: "/login", permanent: false } };
+  }
+
+  const userEntry = await prisma.user.findFirst({
+    where: { id: session.user.id },
+  });
+
+  // If submitted then go dashboard
+  if (
+    userEntry &&
+    (userEntry.typeform_response_id === null ||
+      userEntry.typeform_response_id === undefined)
+  ) {
+    return { props: {} };
+  }
   return { redirect: { destination: "/dashboard", permanent: false } };
-
-  // const session = await getServerAuthSession(context);
-
-  // if (!session || !session.user) {
-  //     return { redirect: { destination: "/login", permanent: false } };
-  // }
-
-  // const userEntry = await prisma.user.findFirst({
-  //     where: { id: session.user.id },
-  // });
-
-  // // If submitted then go dashboard
-  // if (
-  //     userEntry &&
-  //     (userEntry.typeform_response_id === null ||
-  //         userEntry.typeform_response_id === undefined)
-  // ) {
-  //     return { props: {} };
-  // }
-  // return { redirect: { destination: "/dashboard", permanent: false } };
 };
 
 export default Apply;
