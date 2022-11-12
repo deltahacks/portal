9452e8b9131d6f5a6640b266a3b977ae2a9f4e4a
Jeffrey Chan <77026758+KaiKitJeffreyChan@users.noreply.github.com>
2022-11-12 17:30:43 -0500

    fix: Added admin page back (#57)
    * added admin page back
    
    * format
    
    * fix types
    
    Co-authored-by: Krish <52761222+Krish120003@users.noreply.github.com>

---
+++ src/pages/admin.tsx
@@ -0,0 +1,31 @@
+import { GetServerSidePropsContext, NextPage } from "next";
+import { getSession } from "next-auth/react";
+import { getServerAuthSession } from "../server/common/get-server-auth-session";
+
+const Admin: NextPage = () => {
+  return (
+    <>
+      Tempor tempor ea ad consectetur consequat pariatur et officia est mollit
+      nostrud.
+    </>
+  );
+};
+
+export const getServerSideProps = async (
+  context: any,
+  cdx: GetServerSidePropsContext
+) => {
+  const session = await getServerAuthSession(context);
+
+  // If the user is not an ADMIN, kick them back to the dashboard
+  if (!session || !session.user || !session.user.role.includes("ADMIN")) {
+    return {
+      redirect: { destination: "/dashboard", permanent: false },
+    };
+  }
+
+  // Otherwise, continue.
+  return { props: {} };
+};
+
+export default Admin;
