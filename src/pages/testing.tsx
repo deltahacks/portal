3b72fa1b0f932c8ebfa1b7a2043a6c84157c3856
Arian Ahmadinejad <35879206+arian81@users.noreply.github.com>
2022-11-17 04:36:08 -0500

    chore: shuffle need to review responses (#61)
    * add review table & create migration
    
    * shuffle data
    
    * shuffling + only need to review responses
    
    * Update reviewers.ts
    
    * working testing page
    
    Co-authored-by: Krish <52761222+Krish120003@users.noreply.github.com>
    Co-authored-by: Jeffrey Chan <77026758+KaiKitJeffreyChan@users.noreply.github.com>

--- src/pages/testing.tsx
+++ src/pages/testing.tsx
@@ -1,44 +1,21 @@
 import { trpc } from "../utils/trpc";
 import type { NextPage } from "next";
+import { prisma } from "../server/db/client";
 
 const Testing: NextPage = () => {
-  const { data, isLoading, fetchNextPage } = trpc.useInfiniteQuery(
-    ["reviewer.getApplications", {}],
-    {
-      getNextPageParam: (lastPage) => lastPage.nextCursor,
-    }
-  );
+  const { data, isLoading } = trpc.useQuery(["reviewer.getApplications"]);
+  trpc.useQuery(["reviewer.getReviewed"]);
   console.log(data, isLoading);
 
   return (
     <>
-      {data?.pages.map((page) => {
-        return page.data.map((application) => {
-          return (
-            <div key={application.response_id}>
-              {application?.firstName} {application?.lastName}{" "}
-              {application.resume}
-              <iframe
-                src={application.resume || " "}
-                itemType="application/pdf"
-                width={400}
-                height={400}
-                loading="lazy"
-              ></iframe>
-              {/*{application.emergencyContactInfo.phoneNumber}*/}
-            </div>
-          );
-        });
+      {data?.data.map((blob, index) => {
+        return (
+          <div key={index}>
+            {blob.firstName} {blob.lastName}
+          </div>
+        );
       })}
-      {isLoading ? null : (
-        <button
-          className="btn btn-primary"
-          // tRPC fetch the next page
-          onClick={() => fetchNextPage()}
-        >
-          Fetch More
-        </button>
-      )}
     </>
   );
 };
