9c69de306300b449f137482f3e4a2e6dd850ac1f
fvcci <fongf2@mcmaster.ca>
2022-11-30 00:17:31 -0500

    supresssed some more warnings

--- src/server/router/reviewers.ts
+++ src/server/router/reviewers.ts
@@ -163,27 +163,28 @@ export const reviewerRouter = createProtectedRouter()
 
       // Convert from TypeFormResponse to TypeFormSubmission
       const converted: TypeFormSubmission[] = data.items.map((item) => {
-        const responsePreprocessing: any = new Map();
+        const responsePreprocessing: Map<string, TypeFormResponseField> =
+          new Map();
         for (const answer of item.answers) {
           responsePreprocessing.set(answer.field.id, answer);
         }
         return {
           response_id: item.response_id,
-          firstName: responsePreprocessing.get("nfGel41KT3dP").text,
-          lastName: responsePreprocessing.get("mwP5oTr2JHgD").text,
+          firstName: responsePreprocessing.get("nfGel41KT3dP")?.text,
+          lastName: responsePreprocessing.get("mwP5oTr2JHgD")?.text,
           birthday: new Date(responsePreprocessing.get("m7lNzS2BDhp1").date),
-          major: responsePreprocessing.get("PzclVTL14dsF").text,
-          school: responsePreprocessing.get("63Wa2JCZ1N3R").text,
-          willBeEnrolled: responsePreprocessing.get("rG4lrpFoXXpL").boolean,
+          major: responsePreprocessing.get("PzclVTL14dsF")?.text,
+          school: responsePreprocessing.get("63Wa2JCZ1N3R")?.text,
+          willBeEnrolled: responsePreprocessing.get("rG4lrpFoXXpL")?.boolean,
           graduationYear: new Date(
-            responsePreprocessing.get("Ez47B6N0QzKY").date
+            responsePreprocessing.get("Ez47B6N0QzKY")?.date
           ),
-          degree: responsePreprocessing.get("035Ul4T9mldq").text,
-          currentLevel: responsePreprocessing.get("3SPBWlps2PBj").text,
-          hackathonCount: responsePreprocessing.get("MyObNZSNMZOZ").text,
-          longAnswer1: responsePreprocessing.get("rCIqmnIUzvAV").text,
-          longAnswer2: responsePreprocessing.get("h084NVJ0kEsO").text,
-          longAnswer3: responsePreprocessing.get("wq7KawPVuW4I").text,
+          degree: responsePreprocessing.get("035Ul4T9mldq")?.text,
+          currentLevel: responsePreprocessing.get("3SPBWlps2PBj")?.text,
+          hackathonCount: responsePreprocessing.get("MyObNZSNMZOZ")?.text,
+          longAnswer1: responsePreprocessing.get("rCIqmnIUzvAV")?.text,
+          longAnswer2: responsePreprocessing.get("h084NVJ0kEsO")?.text,
+          longAnswer3: responsePreprocessing.get("wq7KawPVuW4I")?.text,
           socialLinks: responsePreprocessing.get("CE5WnCcBNEtj")?.text,
           resume: responsePreprocessing
             .get("z8wTMK3lMO00")
@@ -192,23 +193,24 @@ export const reviewerRouter = createProtectedRouter()
               "/api/resumes"
             ),
           extra: responsePreprocessing.get("GUpky3mnQ3q5")?.text,
-          tshirtSize: responsePreprocessing.get("Q9xv6pezGeSc").text,
-          hackerType: responsePreprocessing.get("k9BrMbznssVX").text,
-          hasTeam: responsePreprocessing.get("3h36sGge5G4X").boolean,
+          tshirtSize: responsePreprocessing.get("Q9xv6pezGeSc")?.text,
+          hackerType: responsePreprocessing.get("k9BrMbznssVX")?.text,
+          hasTeam: responsePreprocessing.get("3h36sGge5G4X")?.boolean,
           workShop: responsePreprocessing.get("Q3MisVaz3Ukw")?.text,
-          gender: responsePreprocessing.get("b3sr6g16jGjj").text,
+          gender: responsePreprocessing.get("b3sr6g16jGjj")?.text,
           considerSponserChat:
             responsePreprocessing.get("LzF2H4Fjfwvq")?.boolean,
-          howDidYouHear: responsePreprocessing.get("OoutsXd4RFcR").text,
-          background: responsePreprocessing.get("kGs2PWAnqBI3").text,
+          howDidYouHear: responsePreprocessing.get("OoutsXd4RFcR")?.text,
+          background: responsePreprocessing.get("kGs2PWAnqBI3")?.text,
           emergencyContactInfo: {
-            firstName: responsePreprocessing.get("o5rMp5fj0BMa").text,
-            lastName: responsePreprocessing.get("irlsiZFKVJKD").text,
-            phoneNumber: responsePreprocessing.get("ceNTt9oUhO6Q").phone_number,
+            firstName: responsePreprocessing.get("o5rMp5fj0BMa")?.text,
+            lastName: responsePreprocessing.get("irlsiZFKVJKD")?.text,
+            phoneNumber:
+              responsePreprocessing.get("ceNTt9oUhO6Q")?.phone_number,
             email: responsePreprocessing.get("onIT7bTImlRj")?.email,
           },
-          mlhAgreement: responsePreprocessing.get("F3vbQhObxXFa").boolean,
-          mlhCoc: responsePreprocessing.get("f3ELfiV5gVSs").boolean,
+          mlhAgreement: responsePreprocessing.get("F3vbQhObxXFa")?.boolean,
+          mlhCoc: responsePreprocessing.get("f3ELfiV5gVSs")?.boolean,
         };
       });
       // filter responses to get the ones that need review
