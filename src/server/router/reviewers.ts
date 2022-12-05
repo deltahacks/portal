5479362b45283e1156dfd89a5b4fcf3cb11f4fff
arian81 <ahmadinejadarian@gmail.com>
2022-12-05 17:07:03 -0500

    deleted shuffling

--- src/server/router/reviewers.ts
+++ src/server/router/reviewers.ts
@@ -145,24 +145,6 @@ export const reviewerRouter = createProtectedRouter()
       const res = await fetch(url, options);
       const data: TypeFormResponse = await res.json();
 
-      //shuffle the responses
-      // ((array: Array<any>) => {
-      //   let currentIndex = array.length,
-      //     randomIndex;
-
-      //   while (currentIndex != 0) {
-      //     randomIndex = Math.floor(Math.random() * currentIndex);
-      //     currentIndex--;
-
-      //     [array[currentIndex], array[randomIndex]] = [
-      //       array[randomIndex],
-      //       array[currentIndex],
-      //     ];
-      //   }
-
-      //   return array;
-      // })(data.items);
-
       // Convert from TypeFormResponse to TypeFormSubmission
       const converted: TypeFormSubmission[] = data.items.map((item) => {
         const responsePreprocessing: any = new Map();
