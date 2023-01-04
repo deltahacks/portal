01ef0f4521a561abb6054dec63ce2f128d1d36bd
Jason Huang <jasonhuang20035@gmail.com>
2023-01-04 15:43:07 -0500

    add link to roles in admin page

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -3,6 +3,7 @@ import {
   GetServerSidePropsResult,
   NextPage,
 } from "next";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { rbac } from "../components/RBACWrapper";
 import { getServerAuthSession } from "../server/common/get-server-auth-session";
@@ -13,6 +14,10 @@ const Admin: NextPage = () => {
     <>
       Tempor tempor ea ad consectetur consequat pariatur et officia est mollit
       nostrud.
+      <br />
+      <Link href="roles">
+        <span className="link">Roles</span>
+      </Link>
     </>
   );
 };
