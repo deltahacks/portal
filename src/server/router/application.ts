d2a7871969e03510625faacbd168128432b735cd
Krish <52761222+Krish120003@users.noreply.github.com>
2023-01-04 00:09:53 -0500

    feat: add application status backend

--- src/server/router/application.ts
+++ src/server/router/application.ts
@@ -1,3 +1,4 @@
+import { Status } from "@prisma/client";
 import { z } from "zod";
 import { createProtectedRouter } from "./context";
 
@@ -20,6 +21,43 @@ export const applicationRouter = createProtectedRouter()
       }
       return true;
     },
+  })
+  .query("status", {
+    output: z.string(),
+    async resolve({ctx}) {
+      const user = await prisma?.user.findFirst({
+        where: { id: ctx.session.user.id },
+      });
+      if (!user) {
+        return "NULL";
+      }
+      if (
+        user.typeform_response_id === undefined ||
+        user.typeform_response_id === null
+      ) {
+        return "NULL";
+      }
+
+      return user.status;
+    }
+  })
+  .mutation("rsvp", {
+    async resolve({ctx, input}) {
+      const user = await prisma?.user.findFirst({
+        where: { id: ctx.session.user.id },
+      });
+
+      if (user?.status != Status.ACCEPTED) {
+        throw new Error("Unauthorized call")
+      }
+
+      await prisma?.user.update(
+        {
+          where: { id: ctx.session.user.id },
+          data: {status: Status.RSVP}
+        }
+      )
+    } 
   })
   .mutation("submit", {
     input: z.object({ id: z.string() }),
