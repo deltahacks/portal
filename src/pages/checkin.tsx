f4a0b78167ea68abbb526ec49b6c454f984cbfee
Rachelle DeMan <demanr@mcmaster.ca>
2023-01-08 20:32:56 -0500

    chore: removed unnecessary component

--- src/pages/checkin.tsx
+++ src/pages/checkin.tsx
@@ -15,31 +15,6 @@ const QRReaderDynamic = dynamic(() => import("../components/QrScanner"), {
   ssr: false,
 });
 
-const Popup: React.FC = () => {
-  return (
-    <div>
-      <input type="checkbox" id="my-modal-3" className="modal-toggle" />
-      <div className="modal">
-        <div className="modal-box relative">
-          <label
-            htmlFor="my-modal-3"
-            className="btn btn-circle btn-sm absolute right-2 top-2"
-          >
-            ✕
-          </label>
-          <h3 className="text-lg font-bold">
-            Congratulations random Internet user!
-          </h3>
-          <p className="py-4">
-            You've been selected for a chance to get one year of subscription to
-            use Wikipedia for free!
-          </p>
-        </div>
-      </div>
-    </div>
-  );
-};
-
 const PreCheckedIn: React.FC = () => {
   const [shouldShow, setShouldShow] = useState(false);
   const [QRCode, setQRCode] = useState("NONE");
