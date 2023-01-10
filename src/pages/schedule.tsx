e1fea01ed44bcd7b73d7636ab43e0cfc954abc0d
fvcci <fongf2@mcmaster.ca>
2023-01-10 17:31:16 -0500

    got a working schedule

--- src/pages/schedule.tsx
+++ src/pages/schedule.tsx
@@ -1,10 +1,38 @@
 import { NextPage } from "next";
+import {
+  ScheduleComponent,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  Inject,
+} from "@syncfusion/ej2-react-schedule";
+import * as React from "react";
 
 //
 // docs for the calendar component www.npmjs.com/package/@syncfusion/ej2-react-schedule
 
-const schedule: NextPage = () => {
-  return <>asdfjkl;</>;
+const Schedule: NextPage = () => {
+  const data = [
+    {
+      Id: 1,
+      Subject: "Meeting",
+      StartTime: new Date(2023, 1, 15, 10, 0),
+      EndTime: new Date(2023, 1, 15, 12, 30),
+    },
+  ];
+
+  return (
+    <ScheduleComponent
+      selectedDate={new Date(2023, 1, 15)}
+      eventSettings={{
+        dataSource: data,
+      }}
+    >
+      <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+    </ScheduleComponent>
+  );
 };
 
-export default schedule;
+export default Schedule;
