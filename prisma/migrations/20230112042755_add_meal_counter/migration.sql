d61da7315e3b088600f474adb11320a4f043d85a
Krish <52761222+Krish120003@users.noreply.github.com>
2023-01-11 23:33:56 -0500

    Revert "Create food db migration"
    This reverts commit 49a2e218341c468860663d84559ce4e15aab4191.

--- prisma/migrations/20230112042755_add_meal_counter/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `lastMealTaken` to the `User` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "lastMealTaken" TIMESTAMP(3) NOT NULL,
-ADD COLUMN     "mealsTaken" INTEGER NOT NULL DEFAULT 0;
