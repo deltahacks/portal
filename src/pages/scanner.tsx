00e07d00cf6537b8883440a10c027d7399f9dc3a
Rachelle DeMan <demanr@mcmaster.ca>
2023-01-13 04:34:43 -0500

    feat: added hacker scanner ui

--- src/pages/scanner.tsx
+++ src/pages/scanner.tsx
@@ -64,7 +64,7 @@ const FoodManagerView: React.FC = () => {
           />
         }
       </div>
-      <h3 className="text-md py-1">
+      <h3 className="py-1 text-md">
         QR Value Scanned: <div className="text-2xl font-bold">{QRCode}</div>
       </h3>
       <h1>
@@ -76,10 +76,10 @@ const FoodManagerView: React.FC = () => {
       </h1>
       <h1>food go brr : {isError ? "not food data" : foodData?.mealsTaken}</h1>
 
-      <div className="flex w-full justify-between gap-4">
+      <div className="flex justify-between w-full gap-4">
         <button
           disabled={QRCode === "NONE"}
-          className="btn btn-primary flex-1 border-none text-base font-medium capitalize"
+          className="flex-1 text-base font-medium capitalize border-none btn btn-primary"
           onClick={async () => {
             await foodMutationAdd.mutateAsync(parseInt(QRCode));
             await utils.invalidateQueries(["food.getFood"]);
@@ -89,7 +89,7 @@ const FoodManagerView: React.FC = () => {
         </button>
         <button
           disabled={QRCode === "NONE"}
-          className="btn btn-primary flex-1 border-none text-base font-medium capitalize"
+          className="flex-1 text-base font-medium capitalize border-none btn btn-primary"
           onClick={async () => {
             await foodMutationSub.mutateAsync(parseInt(QRCode));
             await utils.invalidateQueries(["food.getFood"]);
@@ -118,32 +118,53 @@ const HackerView: React.FC = () => {
     <>
       <div>
         {shouldShowScanner ? (
-          <QRReaderDynamic
-            scanDelay={scanDelay}
-            handleScan={(data) => {
-              setQRCode(data);
-              setScanDelay(false);
-              setShouldShowScanner(false);
-            }}
-            lastVal={qrDefer}
-          />
+          <div>
+            <QRReaderDynamic
+              scanDelay={scanDelay}
+              handleScan={(data) => {
+                setQRCode(data);
+                setScanDelay(false);
+                setShouldShowScanner(false);
+              }}
+              lastVal={qrDefer}
+            />
+          </div>
         ) : null}
       </div>
-      <div>
-        <h1>
-          {socialInfo?.firstName},{socialInfo?.lastName}
-        </h1>
-        <h2>
-          {socialInfo?.school},{socialInfo?.degree},{socialInfo?.currentLevel}
-        </h2>
-        <h3>
-          {socialInfo?.socialLinks?.map((link, i) => (
-            <a key={i} className="block text-blue-400" href={link}>
-              {link}
-            </a>
-          ))}
-        </h3>
-        <img src={socialInfo?.image || ""}></img>
+      <div className="w-full">
+        {!shouldShowScanner ? (
+          <div className="flex flex-col sm:flex-row sm:items-center">
+            <div className="flex flex-col">
+              <div>
+                <h1 className="w-full pt-8 text-2xl font-semibold leading-tight text-black dark:text-white sm:py-2 sm:pt-4 sm:text-3xl lg:text-5xl 2xl:text-6xl">
+                  {socialInfo?.firstName},&nbsp;{socialInfo?.lastName}
+                </h1>
+                <h2 className="pt-2 text-xl font-normal dark:text-[#737373] sm:py-2 sm:pt-0 sm:text-2xl lg:pt-2 lg:text-3xl lg:leading-tight 2xl:pt-6 2xl:text-4xl">
+                  {socialInfo?.school},&nbsp;{socialInfo?.degree},&nbsp;
+                  {socialInfo?.currentLevel}
+                </h2>
+              </div>
+              <h3 className="pt-4 text-black dark:text-white lg:pt-6">
+                {socialInfo?.socialLinks?.map((link, i) => (
+                  <a
+                    key={i}
+                    className="block py-2 font-medium transition ease-in-out hover:text-[#833bff] dark:hover:text-[#9575cc] sm:py-4 "
+                    href={link}
+                    target="_blank"
+                  >
+                    <h1 className="text-base sm:text-xl lg:text-2xl ">
+                      {link}
+                    </h1>
+                  </a>
+                ))}
+              </h3>
+            </div>
+            <img
+              className="w-full h-auto max-w-full p-8 md:w-1/2"
+              src={socialInfo?.image || ""}
+            ></img>
+          </div>
+        ) : null}
       </div>
     </>
   );
@@ -171,13 +192,13 @@ const Scanner: NextPage = () => {
       <Head>
         <title>Check In - DeltaHacks 9</title>
       </Head>
-      <div className="drawer drawer-end relative h-full min-h-screen w-full overflow-x-hidden font-montserrat">
+      <div className="relative w-full h-full min-h-screen overflow-x-hidden drawer drawer-end font-montserrat">
         <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content">
           <Background />
           <NavBar />
 
-          <main className="px-7 py-16 sm:px-14 md:w-10/12 lg:pl-20 2xl:w-8/12 2xl:pt-20">
+          <main className="py-16 px-7 sm:px-14 md:w-10/12 lg:pl-20 2xl:w-8/12 2xl:pt-20">
             <h1 className="text-2xl font-semibold leading-tight text-black dark:text-white sm:text-3xl lg:text-5xl 2xl:text-6xl">
               Scanner
             </h1>
@@ -210,7 +231,7 @@ const Scanner: NextPage = () => {
             )}
           </main>
 
-          <footer className="absolute right-0 bottom-0 p-5 md:absolute md:bottom-0">
+          <footer className="absolute bottom-0 right-0 p-5 md:absolute md:bottom-0">
             <SocialButtons />
           </footer>
         </div>
@@ -232,7 +253,7 @@ const Scanner: NextPage = () => {
                 </Link>
               </li>
             </ul>
-            <div className="mx-1 mb-2 flex w-full items-center justify-between">
+            <div className="flex items-center justify-between w-full mx-1 mb-2">
               <ThemeToggle />
               <div>
                 <a className="font-sub mx-2.5 text-sm">
