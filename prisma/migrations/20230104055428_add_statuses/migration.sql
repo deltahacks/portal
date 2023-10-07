016efee19955789510b9c8e45e34394c03d5a7f6
Arian Ahmadinejad <35879206+arian81@users.noreply.github.com>
2023-10-07 12:22:41 -0400

    chore: changes schema to use new cockroachdb wrapper

--- prisma/migrations/20230104055428_add_statuses/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "Status" AS ENUM ('IN_REVIEW', 'REJECTED', 'WAITLISTED', 'ACCEPTED', 'RSVP', 'CHECKED_IN');
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "status" "Status" NOT NULL DEFAULT 'IN_REVIEW';
