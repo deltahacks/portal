dc1b364112ea3a92d86bc4b7cd91d3233800d688
Felix Fong <fvcci@Felixs-14-MacBook-Pro.local>
2023-10-07 11:36:49 -0400

    decoupled a lot of functionality in schedule.tsx

--- src/pages/schedule.tsx
+++ src/pages/schedule.tsx
@@ -29,90 +29,114 @@ const eventColours = [
 //   },
 // ];
 
+const getRandomIntegerFromRange = (start: number, end: number): number => {
+  return Math.floor(Math.random() * (end - start + 1) + start);
+};
+
+const getEventsSchedule = async (): Promise<Event[]> => {
+  const icsSchedule = await parseIcsSchedule();
+  const eventsSchedule = icsSchedule.map((v) => ({
+    ...v,
+    disabled: true,
+    colorId: v.allDay
+      ? 0
+      : getRandomIntegerFromRange(0, eventColours.length - 1),
+  }));
+  return eventsSchedule;
+};
+
+// Remove resource label from scheduler
+const removeResourceLabel = () => {
+  setTimeout(() => {
+    // Add an event listener to popup
+    const popups = document.querySelectorAll(".dx-overlay-content");
+    popups.forEach((popup) => {
+      popup.addEventListener("click", removeResourceLabel);
+    });
+  }, 500);
+
+  setTimeout(() => {
+    const resource = document.querySelector("textarea");
+    const parent8 =
+      resource?.parentElement?.parentElement?.parentElement?.parentElement
+        ?.parentElement?.parentElement?.parentElement?.parentElement;
+    parent8?.removeChild(parent8?.lastElementChild as Node);
+  }, 100);
+};
+
+const getDefaultCurrentDate = (start: Date, end: Date): Date => {
+  const curDate = new Date(Date.now());
+  return curDate < start || new Date("2023-1-15") < curDate
+    ? new Date("2023-1-13")
+    : curDate;
+};
+
+const MyScheduler = ({
+  events,
+  defaultCurrentView,
+}: {
+  events: Event[];
+  defaultCurrentView: string;
+}) => {
+  const [eventStart, eventEnd] = [new Date("2023-1-13"), new Date("2023-1-15")];
+
+  return (
+    <Scheduler
+      className="h-full"
+      dataSource={events}
+      views={["timelineDay", "agenda"]}
+      defaultCurrentView={defaultCurrentView}
+      defaultCurrentDate={getDefaultCurrentDate(eventStart, eventEnd)}
+      cellDuration={60}
+      firstDayOfWeek={0}
+    >
+      <Editing allowAdding={false} />
+      <Resource
+        dataSource={eventColours}
+        fieldExpr="colorId"
+        useColorAsDefault={true}
+      />
+    </Scheduler>
+  );
+};
+
 // docs for the calendar component https://ej2.syncfusion.com/react/documentation/api/schedule/
 const Schedule: NextPage = () => {
   const [events, setEvents] = useState<Event[]>([]);
 
-  // Remove resource label from scheduler
-  const removeResourceLabel = () => {
-    setTimeout(() => {
-      // Add an event listener to popup
-      const popups = document.querySelectorAll(".dx-overlay-content");
-      popups.forEach((popup) => {
-        popup.addEventListener("click", removeResourceLabel);
-      });
-    }, 500);
+  const LoadingText = () => (
+    <div className="py-4 text-4xl font-bold">Loading...</div>
+  );
 
-    setTimeout(() => {
-      const resource = document.querySelector("textarea");
-      const parent8 =
-        resource?.parentElement?.parentElement?.parentElement?.parentElement
-          ?.parentElement?.parentElement?.parentElement?.parentElement;
-      parent8?.removeChild(parent8?.lastElementChild as Node);
-    }, 100);
-  };
+  const DesktopView = () => (
+    <>
+      {events.length === 0 && <LoadingText />}
+      <MyScheduler events={events} defaultCurrentView="agenda" />
+    </>
+  );
 
-  // Load in the tsv into the scheduler
+  const MobileView = () => (
+    <>
+      {events.length === 0 && <LoadingText />}
+      <MyScheduler events={events} defaultCurrentView="timelineDay" />
+    </>
+  );
+
   useEffect(() => {
     (async () => {
-      const data = [
-        ...(await parseIcsSchedule()).map((v) => ({
-          ...v,
-          disabled: true,
-          // Randomize the colour of the event. If allDay then make it white
-          colorId: v.allDay
-            ? 0
-            : Math.floor(Math.random() * (eventColours.length - 1) + 1),
-        })),
-      ];
-      setEvents(data);
+      const eventsSchedule = await getEventsSchedule();
+      setEvents(eventsSchedule);
     })();
   }, []);
-
-  const Schedule = ({ defaultCurrentView }: { defaultCurrentView: string }) => {
-    // If the user is out of range of the event default them to the start date
-    const curDate = new Date(Date.now());
-    const defaultCurrentDate =
-      curDate < new Date("2023-1-13") || new Date("2023-1-15") < curDate
-        ? new Date("2023-1-13")
-        : curDate;
-
-    return (
-      <Scheduler
-        className="h-full"
-        dataSource={events}
-        views={["timelineDay", "agenda"]}
-        defaultCurrentView={defaultCurrentView}
-        defaultCurrentDate={defaultCurrentDate}
-        cellDuration={60}
-        firstDayOfWeek={0}
-      >
-        <Editing allowAdding={false} />
-        <Resource
-          dataSource={eventColours}
-          fieldExpr="colorId"
-          useColorAsDefault={true}
-        />
-      </Scheduler>
-    );
-  };
 
   return (
     <Drawer>
       <div className="flex-auto overflow-hidden" onClick={removeResourceLabel}>
-        {/* desktop view */}
         <div className="h-full pt-5 sm:hidden">
-          {events.length === 0 && (
-            <div className="py-4 text-4xl font-bold">Loading...</div>
-          )}
-          <Schedule defaultCurrentView="agenda" />
+          <DesktopView />
         </div>
-        {/* mobile view */}
         <div className="hidden h-full p-8 sm:block">
-          {events.length === 0 && (
-            <div className="py-4 text-4xl font-bold">Loading...</div>
-          )}
-          <Schedule defaultCurrentView="timelineDay" />
+          <MobileView />
         </div>
       </div>
     </Drawer>
