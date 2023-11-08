0f8a80dbf5f2417bf12c8e1e5c3e24999ea09c91
Krish120003 <krish120003@gmail.com>
2023-11-08 14:52:58 -0500

    fix: remove generated zod types

--- prisma/zod/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./account";
-export * from "./session";
-export * from "./user";
-export * from "./review";
-export * from "./eventlog";
-export * from "./verificationtoken";
-export * from "./dh10application";
