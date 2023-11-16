e48e545792bd48d9b0d5adfdd4dccb70411701a8
Krish120003 <krish120003@gmail.com>
2023-11-16 14:40:46 -0500

    chore: add bg animation back

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,7 +1,7 @@
 const Background = () => {
   return (
     <div className="absolute inset-0 -z-10 h-full w-full overflow-hidden bg-[#eaeaea] dark:bg-[#171717]">
-      <div className="light-gradient dark:dark-gradient absolute inset-0 -left-[50%] -top-[50%] h-[200%] w-[200%] -rotate-12"></div>
+      <div className="light-gradient dark:dark-gradient absolute inset-0 -left-[50%] -top-[50%] h-[200%] w-[200%] -rotate-12 animate-slow-bg"></div>
     </div>
   );
 };
