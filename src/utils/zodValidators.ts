6cc331760cda82fff631cf86abfedff26f465fac
Felix Fong <fvcci@Felixs-14-MacBook-Pro.local>
2023-11-26 18:16:14 -0500

    created field validators for dh10application

---
+++ src/utils/zodValidators.ts
@@ -0,0 +1,23 @@
+export const validateAgeRange = (date: Date, min: number, max = -1) => {
+  // parse date
+  // make sure over 15
+  const now = new Date();
+  const diff = now.getTime() - date.getTime();
+  const age = Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
+  if (max == -1) {
+    return min <= age;
+  }
+  return min <= age && age <= max;
+};
+
+export const validateWordLength = (sentence: string, limit: number) => {
+  return sentence.split(/\s/g).length <= limit;
+};
+
+export const validateIsTrue = (isTrue: boolean) => {
+  return isTrue === true;
+};
+
+export const transformString = (value: string) => {
+  return value ?? null;
+};
