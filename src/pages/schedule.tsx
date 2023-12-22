e6f5c0236c6db512a35d9acbd13f6375b7edc624
Arian Ahmadinejad <35879206+arian81@users.noreply.github.com>
2023-12-22 12:11:58 -0500

    chore: setup using google api

--- src/pages/schedule.tsx
+++ src/pages/schedule.tsx
@@ -4,6 +4,7 @@ import Scheduler, { Editing, Resource } from "devextreme-react/scheduler";
 import { Drawer } from "../components/NavBar";
 import parseIcsSchedule from "../utils/parseIcsSchedule";
 import { Event } from "../types/scheduler";
+import CustomStore from "devextreme/data/custom_store";
 
 import "devextreme/dist/css/dx.dark.css";
 
@@ -29,9 +30,32 @@ const eventColours = [
 //   },
 // ];
 
+const getData = async (_: any, requestOptions: any) => {
+  const GOOGLE_CALENDAR_URL =
+    "https://www.googleapis.com/calendar/v3/calendars/";
+  const CALENDAR_ID =
+    "25aa70904fce4f627895a2548224e21849fa24aed26334a8db8090ffbc8dc613@group.calendar.google.com";
+  const PUBLIC_KEY = "AIzaSyBnNAISIUKe6xdhq1_rjor2rxoI3UlMY7k";
+
+  const dataUrl = [
+    GOOGLE_CALENDAR_URL,
+    CALENDAR_ID,
+    "/events?key=",
+    PUBLIC_KEY,
+  ].join("");
+  console.log(dataUrl);
+
+  const response = await fetch(dataUrl, requestOptions);
+
+  const data = await response.json();
+
+  return data.items;
+};
+
 // docs for the calendar component https://ej2.syncfusion.com/react/documentation/api/schedule/
 const Schedule: NextPage = () => {
   const [events, setEvents] = useState<Event[]>([]);
+  const [data, setData] = useState<any>(CustomStore);
 
   // Remove resource label from scheduler
   const removeResourceLabel = () => {
@@ -54,33 +78,43 @@ const Schedule: NextPage = () => {
 
   // Load in the tsv into the scheduler
   useEffect(() => {
-    (async () => {
-      const data = [
-        ...(await parseIcsSchedule()).map((v) => ({
-          ...v,
-          disabled: true,
-          // Randomize the colour of the event. If allDay then make it white
-          colorId: v.allDay
-            ? 0
-            : Math.floor(Math.random() * (eventColours.length - 1) + 1),
-        })),
-      ];
-      setEvents(data);
-    })();
+    // (async () => {
+    //   const data = [
+    //     ...(await parseIcsSchedule()).map((v) => ({
+    //       ...v,
+    //       disabled: true,
+    //       // Randomize the colour of the event. If allDay then make it white
+    //       colorId: v.allDay
+    //         ? 0
+    //         : Math.floor(Math.random() * (eventColours.length - 1) + 1),
+    //     })),
+    //   ];
+    //   console.log(data);
+    //   setEvents(data);
+    // })();
+    // const dataSource = new CustomStore({
+    //   load: (options) => getData(options, { showDeleted: false }),
+    // });
+    const dataSource = new CustomStore({
+      load: (options) => getData(options, { showDeleted: false }),
+    });
+    setData(dataSource); // Replace `setData(dataSource);` with `setData([]);`
+    console.log(data);
   }, []);
 
   const Schedule = ({ defaultCurrentView }: { defaultCurrentView: string }) => {
     // If the user is out of range of the event default them to the start date
     const curDate = new Date(Date.now());
-    const defaultCurrentDate =
-      curDate < new Date("2023-1-13") || new Date("2023-1-15") < curDate
-        ? new Date("2023-1-13")
-        : curDate;
+    // const defaultCurrentDate =
+    //   curDate < new Date("2023-1-13") || new Date("2023-1-15") < curDate
+    //     ? new Date("2023-1-13")
+    //     : curDate;
+    const defaultCurrentDate = curDate;
 
     return (
       <Scheduler
         className="h-full"
-        dataSource={events}
+        dataSource={data}
         views={["timelineDay", "agenda"]}
         defaultCurrentView={defaultCurrentView}
         defaultCurrentDate={defaultCurrentDate}
