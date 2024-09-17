7aa1f05b2966a4e52aaf824093a8d0105bf05881
Felix Fong <fongf2@mcmaster.ca>
2024-09-16 22:50:38 -0400

    fix: added prisma migration for the new schema

---
+++ prisma/migrations/20240917024204_dynamic_form_builder/migration.sql
@@ -0,0 +1,103 @@
+-- CreateTable
+CREATE TABLE "FormSubmission" (
+    "id" STRING NOT NULL,
+    "lastUpdated" TIMESTAMP(3) NOT NULL,
+    "isSubmitted" BOOL NOT NULL,
+    "formYear" INT4 NOT NULL,
+
+    CONSTRAINT "FormSubmission_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "FormStructureQuestion" (
+    "displayPriority" INT4 NOT NULL,
+    "formYear" INT4 NOT NULL,
+    "questionId" STRING NOT NULL,
+
+    CONSTRAINT "FormStructureQuestion_pkey" PRIMARY KEY ("formYear","questionId")
+);
+
+-- CreateTable
+CREATE TABLE "FormStructure" (
+    "year" INT4 NOT NULL,
+
+    CONSTRAINT "FormStructure_pkey" PRIMARY KEY ("year")
+);
+
+-- CreateTable
+CREATE TABLE "Answer" (
+    "id" STRING NOT NULL,
+    "statement" STRING NOT NULL,
+    "questionId" STRING NOT NULL,
+    "submissionId" STRING NOT NULL,
+
+    CONSTRAINT "Answer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Question" (
+    "id" STRING NOT NULL,
+    "statement" STRING NOT NULL,
+    "displayOrder" INT4 NOT NULL,
+    "categoryId" STRING NOT NULL,
+    "answerTypeId" STRING NOT NULL,
+
+    CONSTRAINT "Question_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AnswerType" (
+    "name" STRING NOT NULL,
+    "validElements" JSONB,
+
+    CONSTRAINT "AnswerType_pkey" PRIMARY KEY ("name")
+);
+
+-- CreateTable
+CREATE TABLE "QuestionCategory" (
+    "name" STRING NOT NULL,
+
+    CONSTRAINT "QuestionCategory_pkey" PRIMARY KEY ("name")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "FormSubmission_formYear_key" ON "FormSubmission"("formYear");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "FormStructureQuestion_formYear_key" ON "FormStructureQuestion"("formYear");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "FormStructureQuestion_questionId_key" ON "FormStructureQuestion"("questionId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Answer_questionId_key" ON "Answer"("questionId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Answer_submissionId_key" ON "Answer"("submissionId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Question_categoryId_key" ON "Question"("categoryId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Question_answerTypeId_key" ON "Question"("answerTypeId");
+
+-- AddForeignKey
+ALTER TABLE "FormSubmission" ADD CONSTRAINT "FormSubmission_formYear_fkey" FOREIGN KEY ("formYear") REFERENCES "FormStructure"("year") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FormStructureQuestion" ADD CONSTRAINT "FormStructureQuestion_formYear_fkey" FOREIGN KEY ("formYear") REFERENCES "FormStructure"("year") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FormStructureQuestion" ADD CONSTRAINT "FormStructureQuestion_questionId_fkey" FOREIGN KEY ("questionId") REFERENCES "Question"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Answer" ADD CONSTRAINT "Answer_questionId_fkey" FOREIGN KEY ("questionId") REFERENCES "Question"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Answer" ADD CONSTRAINT "Answer_submissionId_fkey" FOREIGN KEY ("submissionId") REFERENCES "FormSubmission"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Question" ADD CONSTRAINT "Question_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "QuestionCategory"("name") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Question" ADD CONSTRAINT "Question_answerTypeId_fkey" FOREIGN KEY ("answerTypeId") REFERENCES "AnswerType"("name") ON DELETE CASCADE ON UPDATE CASCADE;
