05d895131dc3f7bb37b619d36d435c47bf40370b
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2024-10-21 17:26:59 -0400

    fix: make word count ux better

--- src/pages/apply.tsx
+++ src/pages/apply.tsx
@@ -114,8 +114,12 @@ const FormCheckbox: React.FC<
 
 const FormTextArea: React.FC<
   FormInputProps &
-    React.HTMLProps<HTMLTextAreaElement> & { currentLength: number }
-> = ({ label, id, errors, optional, currentLength, register, ...props }) => {
+    React.HTMLProps<HTMLTextAreaElement> & {
+      value: string;
+    }
+> = ({ label, id, errors, optional, value, register, ...props }) => {
+  const wordLength = value?.trim()?.split(/\s/g).length ?? 0;
+  const currentLength = value?.trim()?.length ?? 0;
   return (
     <div className="flex flex-col flex-1 gap-2 pb-4">
       <label className="text-black dark:text-white" htmlFor={id}>
@@ -128,12 +132,12 @@ const FormTextArea: React.FC<
         <div
           className={
             "pt-4 " +
-            (currentLength > 150
+            (wordLength > 150
               ? "text-red-500"
               : "text-neutral-500 dark:text-neutral-400")
           }
         >
-          {150 + 1 - currentLength} words left
+          {151 - wordLength - (currentLength > 0 ? 1 : 0)} words left
         </div>
       </label>
       <textarea
@@ -443,51 +447,44 @@ const ApplyForm = ({
         label="Describe an incident that reshaped your approach to teamwork, leadership, or maintaining a positive outlook."
         errors={errors.longAnswerIncident}
         register={register}
-        currentLength={watch("longAnswerIncident")?.split(/\s/g).length ?? 0}
+        value={watch("longAnswerIncident")}
       />
       <FormTextArea
         id="longAnswerGoals"
         label="How will you make the most out of your experience at DeltaHacks 11, and how will attending the event help you achieve your long-term goals?"
         errors={errors.longAnswerGoals}
         register={register}
-        currentLength={watch("longAnswerGoals")?.split(/\s/g).length ?? 0}
+        value={watch("longAnswerGoals")}
       />
       <FormTextArea
         id="longAnswerFood"
         label="What's your go-to comfort food?"
         errors={errors.longAnswerFood}
         register={register}
-        currentLength={watch("longAnswerFood")?.split(/\s/g).length ?? 0}
+        value={watch("longAnswerFood")}
       />
       <FormTextArea
         id="longAnswerTravel"
         label="If you could travel anywhere in the universe, where would you go and why?"
         errors={errors.longAnswerTravel}
         register={register}
-        currentLength={watch("longAnswerTravel")?.split(/\s/g).length ?? 0}
+        value={watch("longAnswerTravel")}
       />
       <FormTextArea
         id="longAnswerSocratica"
         label="If you did not have to worry about school/money/time, what is the one thing you would work on?"
         errors={errors.longAnswerSocratica}
         register={register}
-        currentLength={watch("longAnswerSocratica")?.split(/\s/g).length ?? 0}
+        value={watch("longAnswerSocratica")}
       />
       <FormDivider label="Survey" />
-      {/* <FormInput
-        label="What are your social media links?"
-        id={"socialText"}
-        errors={errors?.socialText && errors.socialText[0]}
-        register={register}
-        optional
-      /> */}
       <SocialLinksFormInput register={register} errors={errors} watch={watch} />
       <FormTextArea
         label="Is there anything else interesting you want us to know or see?"
         id={"interests"}
         errors={errors.interests}
         register={register}
-        currentLength={watch("interests")?.split(/\s/g).length ?? 0}
+        value={watch("interests") ?? ""}
         optional
       />
       <div className="flex flex-col gap-2 pb-4">
