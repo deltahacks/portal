f44c75b0702c92ae624735f3a342c5f294778918
Krish <krish120003@gmail.com>
2025-10-23 19:09:13 -0400

    fix: lowercaase FAQ (#369)
    * fix: lowercaase FAQ
    
    * chore: lint

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -43,7 +43,7 @@ const Content = () => {
           asChild
           className="btn w-48 border-none hover: hover:bg-zinc-700 text-base font-medium capitalize"
         >
-          <Link href="https://deltahacks.com/#FAQ">FAQ</Link>
+          <Link href="https://deltahacks.com/#faq">FAQ</Link>
         </Button>
       </div>
     </main>
