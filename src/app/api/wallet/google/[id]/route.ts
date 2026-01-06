05b561376c8c010960236dda35b2f3f1ff1f3259
Krish120003 <krish120003@gmail.com>
2026-01-05 19:34:02 -0500

    style: lint

--- src/app/api/wallet/google/[id]/route.ts
+++ src/app/api/wallet/google/[id]/route.ts
@@ -19,7 +19,7 @@ const client = google.walletobjects({
 
 export async function GET(
   _: Request,
-  { params }: { params: Promise<{ id: string }> }
+  { params }: { params: Promise<{ id: string }> },
 ) {
   const userId = (await params).id;
   const session = await getServerSession(authOptions);
@@ -49,7 +49,7 @@ export async function GET(
 
   const eventTicketClass = createClass(
     env.GOOGLE_WALLET_ISSUER_ID,
-    env.GOOGLE_WALLET_CLASS_ID
+    env.GOOGLE_WALLET_CLASS_ID,
   );
 
   await syncClass(eventTicketClass);
@@ -57,7 +57,7 @@ export async function GET(
   const newObject = createObject(
     env.GOOGLE_WALLET_ISSUER_ID,
     env.GOOGLE_WALLET_CLASS_ID,
-    user
+    user,
   );
 
   await syncObject(newObject, userId);
@@ -91,7 +91,7 @@ export async function GET(
 }
 
 async function syncClass(
-  walletClass: walletobjects_v1.Schema$EventTicketClass
+  walletClass: walletobjects_v1.Schema$EventTicketClass,
 ) {
   let classExists = false;
   try {
@@ -133,7 +133,7 @@ async function syncClass(
 
 function createClass(
   issuerId: string,
-  classId: string
+  classId: string,
 ): walletobjects_v1.Schema$EventTicketClass {
   return {
     id: `${issuerId}.${classId}`,
@@ -195,7 +195,7 @@ function createClass(
 // again
 async function syncObject(
   walletObject: walletobjects_v1.Schema$EventTicketObject,
-  userId: string
+  userId: string,
 ) {
   let objectExists = false;
   try {
@@ -235,7 +235,7 @@ async function syncObject(
 function createObject(
   issuerId: string,
   classId: string,
-  user: User & { DH12Application: DH12Application | null }
+  user: User & { DH12Application: DH12Application | null },
 ): walletobjects_v1.Schema$EventTicketObject {
   return {
     id: `${issuerId}.${user.id}`,
