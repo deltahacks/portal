7150feecfdf5fc0f8936fe74f03593dd5ada3f12
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2026-01-07 01:01:23 -0500

    Update src/server/router/scanner.ts
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

--- src/server/router/scanner.ts
+++ src/server/router/scanner.ts
@@ -166,13 +166,12 @@ export const scannerRouter = router({
       if (stationId === "checkIn") {
         // This code is intentionally explicit so it's easy to trace what happens to each status
         if (
-          user.DH12Application.status in
           [
             Status.IN_REVIEW,
             Status.REJECTED,
             Status.WAITLISTED,
             Status.ACCEPTED, // This might look confusing but a user who didn't RSVP is also considered no accepted
-          ]
+          ].includes(user.DH12Application.status)
         ) {
           throw new TRPCError({
             code: "UNAUTHORIZED",
