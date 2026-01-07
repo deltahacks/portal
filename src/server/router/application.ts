f13508eae03cd55d3c1e9a6b8b41b3b202ec409e
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2026-01-07 17:22:53 -0500

    fix: remove status from user schema (#419)
    * fix: remove status from user schema
    
    * fix: update endpoint to use correct status
    
    * fix: remove references to old status
    
    * fix: remove status change

--- src/server/router/application.ts
+++ src/server/router/application.ts
@@ -346,10 +346,6 @@ export const applicationRouter = router({
       await ctx.prisma.dH12Application.delete({
         where: { id: user.DH12ApplicationId },
       });
-      await ctx.prisma.user.update({
-        where: { id: ctx.session.user.id },
-        data: { status: Status.IN_REVIEW }, // Replace with the correct status
-      });
       // create logsnag log
       await ctx.logsnag.track({
         channel: "applications",
